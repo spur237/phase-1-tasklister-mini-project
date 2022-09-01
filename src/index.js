80ecef1427eb2921a586e1258ad53160addbe2da
spur237 <samuel.nzioka@student.moringaschool.com>
2022-09-01 15:44:48 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.querySelector("#id-list").addEventListener("click", (event) => 
+  {
+    document.getElementById("create-task-form").innerHTML += "Sorry! preventDefault!";
+    event.preventDefault();
+  }, false); 
